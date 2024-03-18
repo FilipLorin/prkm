14c5a3cf924055c2a2dc99c23818c18136402477
Mr-Moose-dev <filip.lorin@gmail.com>
2024-03-18 18:02:27 +0100

    vectors

--- lib.py
+++ lib.py
@@ -1,3 +1,15 @@
-import numpy as np
+class GlobalPosVector:
+    def __init__(self, x, y, z, a):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.a = a
+
+class JointPosVector:
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
 
 
