f65b6511517832d05c287e0e0fda83304eef7bd6
Mr-Moose-dev <filip.lorin@gmail.com>
2024-05-14 13:08:49 +0200

    daily

--- lib.py
+++ lib.py
@@ -11,22 +11,39 @@ class Vec:
 
 @dataclass
 class Robot:
-    bZO: float
-    bR: float
-    a: float
-    l: float
-    fR: float
-    tH: float
-    baseVec: Vec
-    toolVec: Vec
+    bZO:float
+    bR:float
+    a:float
+    l:float
+    fR:float
+    tH:float
+    baseVec:Vec
+    toolVec:Vec
+
+    def attach_tool(self, tool:Vec):
+        self.toolVec = tool
+
+    def attach_base(self, base:Vec):
+        self.baseVec = base
 
 
-def readDimentions(fileName:str="dimentions.json", id:str="robot"):
-    with open(fileName, "r") as file:
-        data = json.loads(file.read())
-    #TODO: Data validation
-    return data[id]
+class RobotFactory:
+    def _deserialise_from_json(self, file_name:str):
+        with open(file_name, "r") as file:
+            data = json.loads(file.read())
+        return data
 
+    def _get_deserialiser(self, format:str):
+        if format == 'JSON':
+            return self._deserialise_from_json
+        else:
+            raise ValueError(fromat)
 
-def setup():
-    pass
+    def deserialise(self, format:str, file_name:str):
+        deserialiser = self._get_deserialiser(format)
+        #TODO: Data validation
+        return deserialiser(file_name, id)
+
+    def create_from_file(self, format:str, file_name:str):
+        data = deserialise(format, file_name)
+    
