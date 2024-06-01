3697178990e784cb0673911114e72426e02e69ec
Mr-Moose-dev <filip.lorin@gmail.com>
2024-06-01 20:52:12 +0200

    implem_fwd

--- lib.py
+++ lib.py
@@ -12,7 +12,6 @@ class Robot:
     L:float
     l:float
     wP:float
-    tH:float
 
     def __post_init__(self):
         self.toolVec = np.array([0,0,0])
@@ -30,9 +29,9 @@ class Robot:
         else:
             return b
 
-    def _internal_inverse_kinematics(self, x:float, y:float, z:float, w:float):
+    def _internal_inverse_kinematics(self, x:float, y:float, z:float):
         a = self.wB - 2*self.wP
-        b = 3*math.sqrt(3)*self.wP - (math.sqrt(3)/2)*self.wB
+        b = (3/math.sqrt(3))*self.wP - (math.sqrt(3)/2)*self.wB
         c = self.wP - 0.5*self.wB
 
         E1 = 2*self.L*(y+a)
@@ -60,13 +59,90 @@ class Robot:
 
         #TODO: check for invalid data
 
-        return [th1, th2, th3, w]
+        return [th1, th2, th3]
 
     def inverse_kinematics(self, x, y, z, w):
-        ExtP = np.array([x,y,z])
+        ExtP = np.array([x, y, z])
+        BOVec = np.array([0, 0, -self.bZO])
         ToolZRot = np.array([[math.cos(w), -math.sin(w), 0], [math.sin(w), math.cos(w), 0], [0, 0, 1]])
-        I = self.baseVec - ExtP + ToolZRot.dot(self.toolVec)
-        return self._internal_inverse_kinematics(I[0], I[1], I[2], w)
+        I = self.baseVec + BOVec - ExtP + ToolZRot.dot(self.toolVec)
+        return list(map(lambda x: round(x,6), [*self._internal_inverse_kinematics(I[0], I[1], I[2]), w]))
+
+    def _internal_forward_kinematics(self, th1, th2, th3):
+        # knee joints' coordinates
+        A1 = np.array([0, 
+                       -self.wB-self.L*math.cos(th1)+2*self.wP, 
+                       -self.L*math.sin(th1)])
+        A2 = np.array([0.5*math.sqrt(3)*(self.wB+self.L*math.cos(th2))-(3/math.sqrt(3))*self.wP, 
+                       0.5*(self.wB+self.L*math.cos(th2))-self.wP, 
+                       -self.L*math.sin(th2)])
+        A3 = np.array([-0.5*math.sqrt(3)*(self.wB+self.L*math.cos(th3))+(3/math.sqrt(3))*self.wP,
+                       0.5*(self.wB+self.L*math.cos(th3))-self.wP,
+                       -self.L*math.sin(th3)])
+        
+        # three spheres intersection algorythm
+        
+        #  same z hegiht guard clause
+        if (math.isclose(A1[2], A2[2]) and math.isclose(A2[2], A3[2])):
+            #print("Z-Heights close, switching to alternative solver")
+            a = 2*(A3[0]-A1[0])
+            b = 2*(A3[1]-A1[1])
+            c = A3[0]**2+A3[1]**2-A1[0]**2-A1[1]**2
+            d = 2*(A3[0]-A2[0])
+            e = 2*(A3[1]-A2[1])
+            f = A3[0]**2+A3[1]**2-A2[0]**2-A2[1]**2
+
+            x = (c*e-b*f)/(a*e-b*d)
+            y = (a*f-c*d)/(a*e-b*d)
+
+            B = -2*A1[2]
+            C = A1[2]**2-self.l**2+(x-A1[0])**2+(y-A1[1])**2
+
+            z1 = 0.5*(-B+math.sqrt(B**2-4*C))
+            z2 = 0.5*(-B-math.sqrt(B**2-4*C))
+
+            return [x, y, min(z1, z2)]
+
+
+        a1_ = 2*(A3-A1)
+        a2_ = 2*(A3-A2)
+        b1 = A3.dot(A3) - A1.dot(A1)
+        b2 = A3.dot(A3) - A2.dot(A2)
+        
+        a1 = a1_[0]/a1_[2]-a2_[0]/a2_[2]
+        a2 = a1_[1]/a1_[2]-a2_[1]/a2_[2]
+        a3 = b2/a2_[2]-b1/a1_[2]
+        a4 = -a2/a1
+        a5 = -a3/a1
+        a6 = (-a2_[0]*a4-a2_[1])/(a2_[2])
+        a7 = (b2-a2_[0]*a5)/(a2_[2])
+
+        a = a4**2+1+a6**2
+        b = 2*a4*(a5-A1[0])-2*A1[1]+2*a6*(a7-A1[2])
+        c = a5*(a5-2*A1[0])+a7*(a7-2*A1[2])+A1.dot(A1)-self.l**2
+
+        # forcing downmost solution
+        y1 = (-b+math.sqrt(b**2-4*a*c))/(2*a)
+        y2 = (-b-math.sqrt(b**2-4*a*c))/(2*a)
+
+        x1 = a4*y1+a5
+        z1 = a6*y1+a7
+        x2 = a4*y2+a5
+        z2 = a6*y2+a7
+
+        if z1 < z2:
+            return [x1, y1, z1]
+        else:
+            return [x2, y2, z2]
+
+
+    def forward_kinematics(self, th1, th2, th3, phi):
+        BOVec = np.array([0, 0, -self.bZO])
+        ToolZRot = np.array([[math.cos(phi), -math.sin(phi), 0], [math.sin(phi), math.cos(phi), 0], [0, 0, 1]])
+        I = np.array(self._internal_forward_kinematics(th1, th2, th3))
+        ExtP = self.baseVec + BOVec + I + ToolZRot.dot(self.toolVec)
+        return list(map(lambda x: round(x,6), [ExtP[0], ExtP[1], ExtP[2], phi]))
+
 
 
 class RobotFactory:
@@ -89,7 +165,7 @@ class RobotFactory:
     def _str2arr(self, val):
        return np.array(val.split(", ")) 
 
-    def create_from_file(self, format:str = 'JSON', file_name:str = 'dimentions.json'):
+    def create_from_file(self, format:str, file_name:str):
         data = self.deserialise(format, file_name)
         #TODO: Data validation
         try:
--- prkm.py
+++ prkm.py
@@ -5,15 +5,21 @@ parser = argparse.ArgumentParser(prog="python prkm.py")
 parser.add_argument('-f', '--file', help='name of JSON file with robot dimentions', required=True)
 subparsers = parser.add_subparsers(required=True, help='type kinematic solution to find') 
 
-inverse_parser = subparsers.add_parser('inverse', help='find inverse kinematics solution for a given tooltip position',
-                                       description = 'Returns the theta_1-3 and phi joint angles. Theta=0 when horisontal, positive when downwards. Phi = 0 ...')
+inverse_parser = subparsers.add_parser('inverse', aliases = ['inv'], help='find inverse kinematics solution for a given tooltip position',
+                                       description = """Returns the theta_1-3 and phi joint angles. 
+                                       \n Theta=0 when horisontal, positive when downwards. 
+                                       \n Phi = 0 ...""")
 inverse_parser.add_argument('x', type=float, help='global X coordinate of tooltip to solve for')
 inverse_parser.add_argument('y', type=float, help='global Y ...')
 inverse_parser.add_argument('z', type=float, help='global Z ...')
-inverse_parser.add_argument('a', type=float, help='')
+inverse_parser.add_argument('a', type=float, help='tool orientation (Z rotation)')
 inverse_parser.set_defaults(task='inverse')
 
-forward_parser = subparsers.add_parser('forward', help='find forward kinematics solution for given joint positions')
+forward_parser = subparsers.add_parser('forward',aliases = ['fwd'], help='find forward kinematics solution for given joint positions')
+forward_parser.add_argument('th1', type=float, help = 'angle in join 1')
+forward_parser.add_argument('th2', type=float, help = 'angle in join 2')
+forward_parser.add_argument('th3', type=float, help = 'angle in join 3')
+forward_parser.add_argument('phi', type=float, help = 'angle in join 4')
 forward_parser.set_defaults(task='forward')
 
 args = parser.parse_args()
@@ -23,3 +29,5 @@ robot = factory.create_from_file('JSON', args.file)
 
 if(args.task=='inverse'):
     print(robot.inverse_kinematics(args.x, args.y, args.z, args.a))
+elif(args.task=='forward'):
+    print(robot.forward_kinematics(args.th1, args.th2, args.th3, args.phi))
